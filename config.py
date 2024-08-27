3c8742b9691d1d9145543b367d36d1cb235f1afd
Andromeda <andrei.froschgaizer@yandex.ru>
2024-08-27 15:48:00 +0700

    добавлен конфиг

---
+++ config.py
@@ -0,0 +1,13 @@
+EDGE_DRIVER_PATH='./msedgedriver1.exe'
+
+START_URL = "https://www.selenium.dev/selenium/web/web-form.html"
+SECOND_URL = "https://intoli.com/blog/not-possible-to-block-chrome-headless/chrome-headless-test.html"
+
+TELEGRAM_BOT_TOKEN = '7051587882:AAEtjuuz9eohyNrqoWZ30dSbiJKiPB-oAOM'
+TELEGRAM_CHAT_ID = '438055440'
+
+TEST_ACCOUNT_USERNAME = 'your-test-account-username'
+TEST_ACCOUNT_PASSWORD = 'your-test-account-password'
+
+APPLICATION_RUN_TIME = '10:00'
+SEND_STATUS_NOTIFICATION_TIME='14:00'
