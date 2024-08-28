cd4d2331548abc48853ea9c8f492b59232b0701a
Andromeda <andrei.froschgaizer@yandex.ru>
2024-08-28 18:08:17 +0700

    обновлен конфиг, добавлен пример env

--- config.py
+++ config.py
@@ -1,19 +1,32 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
 # Загружаем переменные из .env файла
 load_dotenv()
 
-EDGE_DRIVER_PATH = os.getenv('EDGE_DRIVER_PATH')
+# Флаг для режима разработки
+IS_DEV = os.getenv('IS_DEV', 'false').lower() == 'true'
 
-START_URL = os.getenv('START_URL')
-SECOND_URL = os.getenv('SECOND_URL')
+# Путь к драйверу
+EDGE_DRIVER_PATH = os.getenv("EDGE_DRIVER_PATH")
 
-TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+START_URL = os.getenv("START_URL")
+SECOND_URL = os.getenv("SECOND_URL")
 
-TEST_ACCOUNT_USERNAME = os.getenv('TEST_ACCOUNT_USERNAME')
-TEST_ACCOUNT_PASSWORD = os.getenv('TEST_ACCOUNT_PASSWORD')
+# Информация для Telegram-бота
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-APPLICATION_RUN_TIME = os.getenv('APPLICATION_RUN_TIME')
-SEND_STATUS_NOTIFICATION_TIME = os.getenv('SEND_STATUS_NOTIFICATION_TIME')
+# API-ключ для 365sms
+API_365SMS_KEY = os.getenv("API_365SMS_KEY")
+
+# Пароль для нового аккаунта
+ACCOUNT_PASSWORD = os.getenv("ACCOUNT_PASSWORD")
+
+# Данные для входа в тестовый аккаунт
+TEST_ACCOUNT_USERNAME = os.getenv("TEST_ACCOUNT_USERNAME")
+TEST_ACCOUNT_PASSWORD = os.getenv("TEST_ACCOUNT_PASSWORD")
+
+# Время запуска приложения и отправки уведомлений
+APPLICATION_RUN_TIME = os.getenv("APPLICATION_RUN_TIME")
+SEND_STATUS_NOTIFICATION_TIME = os.getenv("SEND_STATUS_NOTIFICATION_TIME")
