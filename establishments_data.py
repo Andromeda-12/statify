0fd730bcedccc71fff22e96ab4663d8498bc82bd
Andromeda <andrei.froschgaizer@yandex.ru>
2024-08-30 18:23:21 +0700

    тестовые данные заведений

---
+++ establishments_data.py
@@ -0,0 +1,20 @@
+establishments_data = [
+    {
+        "name": "Старый город",
+        "niche": "Рестораны",
+        "coordinates": {"latitude": "55.772193", "longitude": "37.660705"},
+        "repeats": 6,
+    },
+    {
+        "name": "Катишэ",
+        "niche": "Салон красоты",
+        "coordinates": {"latitude": "55.775722", "longitude": "37.533113"},
+        "repeats": 4,
+    },
+    {
+        "name": "Inlove Beauty",
+        "niche": "Косметология",
+        "coordinates": {"latitude": "55.762049", "longitude": "37.634247"},
+        "repeats": 2,
+    },
+]
