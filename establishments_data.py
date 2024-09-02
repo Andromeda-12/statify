46cace82e4ffca3d5cd28687beff1fb3e3c6d759
Andromeda <andrei.froschgaizer@yandex.ru>
2024-09-02 18:48:14 +0700

    добавлены адреса для тестовых данных

--- establishments_data.py
+++ establishments_data.py
@@ -1,20 +1,30 @@
 establishments_data = [
     {
+        "name": "Мята Lounge",
+        "niche": "Кальян-бар",
+        "coordinates": {"latitude": "52.680707", "longitude": "37.715830"},
+        "address": "ул. Гурьянова, 30, Москва, этаж 2",
+        "repeats": 1,
+    },
+    {
         "name": "Старый город",
         "niche": "Рестораны",
         "coordinates": {"latitude": "55.772193", "longitude": "37.660705"},
-        "repeats": 6,
+        "address": "Новорязанская ул., 16/11с1, Москва, центральный вход, этаж 1",
+        "repeats": 1,
     },
     {
         "name": "Катишэ",
         "niche": "Салон красоты",
         "coordinates": {"latitude": "55.775722", "longitude": "37.533113"},
-        "repeats": 4,
+        "address": "Хорошёвское ш., 11, Москва, Справа от 4го подъезда, отдельный вход!",
+        "repeats": 1,
     },
     {
         "name": "Inlove Beauty",
         "niche": "Косметология",
         "coordinates": {"latitude": "55.762049", "longitude": "37.634247"},
-        "repeats": 2,
+        "address": "Мясницкая ул., 22, стр. 1, Москва, подъезд 1",
+        "repeats": 1,
     },
 ]
