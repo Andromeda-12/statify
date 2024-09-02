02b22169670cf1bb85a1116db6ff38befe2c35a4
Andromeda <andrei.froschgaizer@yandex.ru>
2024-09-02 18:47:52 +0700

    обновлен конфиг

--- config.py
+++ config.py
@@ -32,14 +32,22 @@ API_365SMS_KEY = os.getenv("API_365SMS_KEY")
 
 # Пароль для нового аккаунта
 ACCOUNT_PASSWORD = os.getenv("ACCOUNT_PASSWORD")
-ACCOUNT_FIRSTNAME = 'Иван'
-ACCOUNT_LASTNAME = 'Иванов'
+ACCOUNT_FIRSTNAME = "Иван"
+ACCOUNT_LASTNAME = "Иванов"
 
 # Данные для входа в тестовый аккаунт
 TEST_ACCOUNT_USERNAME = os.getenv("TEST_ACCOUNT_USERNAME")
 TEST_ACCOUNT_PASSWORD = os.getenv("TEST_ACCOUNT_PASSWORD")
-TEST_ACCOUNT_COOKIE_FILE_NAME = 'test_account_cookie'
+TEST_ACCOUNT_COOKIE_FILE_NAME = "test_account_cookie"
 
 # Время запуска приложения и отправки уведомлений
 APPLICATION_RUN_TIME = os.getenv("APPLICATION_RUN_TIME")
 SEND_STATUS_NOTIFICATION_TIME = os.getenv("SEND_STATUS_NOTIFICATION_TIME")
+
+# Количество попыток, сколько раз пытаемся обработать заведение в случае ошибки
+MAX_PROCESS_ESTABLISHMENTS_ATTEMPTS = 3
+
+# Количество попыток для получения списков заведения
+MAX_GET_ESTABLISHMENTS_LIST_ATTEMPTS = 1
+# Количество попыток для получения целевого заведения в списке заведений
+MAX_GET_TARGET_ESTABLISHMENTS_ATTEMPTS = 1
